e5c1af9ea2b6cd385f14c47424e154750fe4a4d4
XxWhitepegasusxX <alexanderalmeida3040@gmail.com>
2023-03-12 21:36:51 -0300

    urlFor Fixed

--- src/container/Work/index.tsx
+++ src/container/Work/index.tsx
@@ -57,11 +57,11 @@ const Work = () => {
           </div>
         ))}
       </div>
-      <motion.div animate={animateCard} transition={{duration: 0.5, delayChildren: 0.5}} className="app__work-portfolio">
+        <motion.div animate={`${animateCard}`} transition={{duration: 0.5, delayChildren: 0.5}} className="app__work-portfolio">
           {filterWork.map((work, index) => (
             <div className='app__work-item app__flex' key={index}>
               <div className="app__work-img app__flex">
-                <img src={urlFor(work.imgUrl)} alt={work.title} />
+                <img src={urlFor(work.imgUrl) as unknown as string} alt={work.title} />
                 <motion.div whileHover={{opacity: [0, 1]}} transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }} className="app__work-hover app__flex">
                   <a href={work.projectLink} target="_blank" rel="noreffer">
                     <motion.div whileInView={{scale: [0, 1]}} whileHover={{scale: [1, 0.9]}} transition={{ duration: 0.25 }} className="app__flex" >
